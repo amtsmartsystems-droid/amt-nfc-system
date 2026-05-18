777a1fa8921242f9473369ed41ab5bda2b6782c2
ali.mohamaad.turokmani <ali.mohamaad.turokmani@gmail.com>
2026-05-18 22:40:35 +0300

    تحديث رابط التوجيه في الخادم إلى Vercel

--- backend/controllers/redirectController.js
+++ backend/controllers/redirectController.js
@@ -13,7 +13,7 @@ exports.handleRedirect = async (req, res) => {
 
         // إذا كانت غير مفعلة، نوجهه لصفحة التفعيل
         if (!card.isActivated) {
-            return res.redirect(`http://localhost:3000/activate/${shortCode}`);
+            return res.redirect(`res.redirect(`https://amt-nfc-system-q1zi.vercel.app/activate/${shortCode}`);/activate/${shortCode}`);
         }
 
         // إذا كانت مفعلة: نزيد النقرات ونوجهه لرابطه النهائي
