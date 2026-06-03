19db7aaf8022952a1f5d632cfbcffb7830852b28
ali.mohamaad.turokmani <ali.mohamaad.turokmani@gmail.com>
2026-06-03 12:38:09 +0300

    temp: remove env fetcher

--- app/api/temp-env/route.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-  return NextResponse.json({
-    MONGO_URI: process.env.MONGO_URI,
-    GEMINI_API_KEY: process.env.GEMINI_API_KEY,
-    JWT_SECRET: process.env.JWT_SECRET,
-  });
-}
