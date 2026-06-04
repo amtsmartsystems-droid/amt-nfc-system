bbade4b1d9324fd07ab987721804bcfa369d4367
ali.mohamaad.turokmani <ali.mohamaad.turokmani@gmail.com>
2026-06-05 00:45:36 +0300

    fix: correct mongodb import paths in upload and file routes

--- app/api/file/[fileId]/route.js
+++ app/api/file/[fileId]/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import connectDB from '../../../../lib/mongodb';
+import connectDB from '../../../../backend/config/db';
 import mongoose from 'mongoose';
 
 const FileSchema = new mongoose.Schema({
